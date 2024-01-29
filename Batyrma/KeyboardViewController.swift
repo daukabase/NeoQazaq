f6bdd1a54438f9f0a094afcbd0ef333631c968e1
Daulet Almagambetov <daulet.almagambetov@indriver.com>
2024-01-30 00:07:00 +0600

    add Russian keyboard

---
+++ Batyrma/KeyboardViewController.swift
@@ -0,0 +1,73 @@
+//
+//  KeyboardViewController.swift
+//  Batyrma
+//
+//  Created by Daulet Almagambetov on 29.01.2024.
+//
+
+import UIKit
+import SnapKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    lazy var keyboardView: RussianKeyboardView = {
+        let view = RussianKeyboardView()
+        view.delegate = self
+        return view
+    }()
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
+
+extension KeyboardViewController: RussianKeyboardViewDelegate {
+    func russianKeyboardView(_ keyboardView: RussianKeyboardView, didTapKey key: String) {
+        textDocumentProxy.insertText(key)
+    }
+}
---
+++ Batyrma/RussianKeyboardView.swift
@@ -0,0 +1,93 @@
+//
+//  RussianKeyboardView.swift
+//  Batyrma
+//
+//  Created by Daulet Almagambetov on 29.01.2024.
+//
+
+import UIKit
+
+// delegate for key tap
+protocol RussianKeyboardViewDelegate: AnyObject {
+    func russianKeyboardView(_ keyboardView: RussianKeyboardView, didTapKey key: String)
+}
+
+class RussianKeyboardView: UIView {
+    enum Constants {
+        static let russianKeys = [
+            ["Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ"],
+            ["Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э"],
+            ["Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю"]
+        ]
+    }
+    weak var delegate: RussianKeyboardViewDelegate?
+
+    // MARK: - UI
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        return stackView
+    }()
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+    
+    // MARK: - Public Methods
+    func addButtons(_ buttons: [KeyboardButton]) {
+        buttons.forEach { stackView.addArrangedSubview($0) }
+    }
+}
+
+private extension RussianKeyboardView {
+    func setupViews() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(5)
+        }
+
+        for row in Constants.russianKeys {
+            let rowStackView = createRowStackView()
+            for key in row {
+                let keyButton = createKeyButton(title: key)
+                rowStackView.addArrangedSubview(keyButton)
+            }
+            stackView.addArrangedSubview(rowStackView)
+        }
+    }
+
+    func createRowStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        return stackView
+    }
+
+    func createKeyButton(title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.backgroundColor = .lightGray
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(keyPressed), for: .touchUpInside)
+        return button
+    }
+
+    @objc private func keyPressed(_ sender: UIButton) {
+        guard let key = sender.titleLabel?.text else { return }
+        delegate?.russianKeyboardView(self, didTapKey: key)
+    }
+}
+
