ea30ec5a78602462e1fb7614d7c3792f5d4b8bd1
Daulet Almagambetov <daulet.almagambetov@indriver.com>
2024-01-29 23:40:27 +0600

    Initial Commit

---
+++ Qazaqsha/QazaqshaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QazaqshaApp.swift
+//  Qazaqsha
+//
+//  Created by Daulet Almagambetov on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct QazaqshaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
